9160cb66be5f60c49ddc6f8b07715122b5d2285c
Kanimozhi04 <kanimozhi.k@tringapps.com>
2022-05-13 08:23:06 +0530

    crud

---
+++ src/Form.js
@@ -0,0 +1,123 @@
+import React,{useState} from 'react';
+import FormControl from '@mui/material/FormControl';
+import  {deepPurple}  from '@mui/material/colors';
+import {CardContent,CardActions,Card,CardMedia,Typography,Box,Paper,TextField,Button,Avatar} from '@mui/material';
+
+export default function Form()
+{ 
+    const [name,setName]=useState();
+    const [price,setPrice]=useState();
+    const [description,setDescription]=useState();
+
+    const [productdetails,setProductDetails]=useState([]);
+    const [temp,setTemp]=useState("false");
+    const [count,setCount] = useState(1);
+    const [id,setId]=useState();
+  
+    const formSubmit=()=>
+    {  
+        
+        if(temp=="false") 
+        {
+          let details = {id:count,name,price,description};
+          setProductDetails([...productdetails,details])
+          setCount(count+1) 
+         
+        }
+        else 
+        {
+          let replaceValues = productdetails.filter(findValues => findValues.id !== id)
+          let updateValues = {id,name,price,description}
+          setProductDetails([...replaceValues,updateValues])
+        }
+        
+        setId()
+        setName("")
+        setPrice("")
+        setDescription("")
+        setTemp("false")
+        
+    }
+  const remove = (id) =>{
+      let deleteValues = productdetails.filter(findValues => findValues.id !== id)
+      setProductDetails(deleteValues)
+  } 
+
+  const edit = (id) =>{
+      let editValues = productdetails.filter(findValues => findValues.id == id)
+      setId(editValues[0].id)
+      setName(editValues[0].name)
+      setPrice(editValues[0].price)
+      setDescription(editValues[0].description)
+      setTemp("true")
+     
+  }  
+   
+    console.log(productdetails);
+     return(
+      <div>
+        <FormControl style={{display:"flex",justifyContent:"center"}}  >
+        <Box
+        style={{display:"flex",justifyContent:"center"}}
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          '& > :not(style)': {
+            m: 1,
+            width: 628,
+            height: 428,
+            bgcolor:"#76e8c4",
+          },
+        }}
+      >
+       
+        <Paper elevation={3} style={{display:"flex",flexDirection:"column",justifyContent:"space-around",alignItems:"center"}} >
+         <div>
+            <Avatar sx={{ bgcolor: deepPurple[500]  }}>KM</Avatar>
+        </div>
+         <div>
+           <TextField  id="outlined-basic" style={{width:"420px"}} label="Product Name" variant="outlined" value={name} onChange={(namedata)=>setName(namedata.target.value)} />
+        </div>
+        <div>
+           <TextField id="outlned-basic" style={{width:"420px"}} label="Product Price" variant="outlined" value={price} onChange={(pricedata)=>setPrice(pricedata.target.value)} />
+        </div>
+        <div>
+           <TextField id="outlined-basic"  multiline rows={4} style={{width:"420px"}} label="Description"  variant="outlined" value={description} onChange={(descriptiondata)=>setDescription(descriptiondata.target.value)} />
+        </div>
+        <div>
+           <Button variant="contained"  onClick={()=>formSubmit()}>Submit</Button>
+        </div>
+
+        </Paper>
+        
+      </Box>
+     </FormControl>
+        <Box>
+          <div>
+           {
+                productdetails && productdetails.map(Values =>{
+                  return(
+                  <Card sx={{ maxWidth: 345,bgcolor:"#95f089",mb:"1%"}} className='product'>
+                        <CardMedia component="img" height="200" image="dog.jpg"alt="dog" />
+                    
+                      <CardContent>
+                        <Typography gutterBottom variant="h6">Product Name:{Values.name}</Typography>
+                        <Typography gutterBottom variant="h6">Product Price:{Values.price}</Typography>
+                        <Typography gutterBottom variant="h6">Product Description:{Values.description}</Typography>
+
+                     </CardContent>
+                     <CardActions> 
+                        <Button size="small"  onClick={() => edit(Values.id)}>Edit</Button>
+                        <Button size="small"  onClick={() => remove(Values.id)}>Delete</Button>
+                     </CardActions>
+                  </Card>
+                
+                  )
+                })
+            }
+          </div>
+        </Box>
+    
+     </div> 
+    );
+}
